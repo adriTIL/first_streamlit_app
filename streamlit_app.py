cdba0b300b6d51611e81902808f89f72fdc6f2ff
adriTIL <138571212+adriTIL@users.noreply.github.com>
2023-09-07 11:57:13 +0200

    Update streamlit_app.py
    back to changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,2 @@
 import streamlit
-streamlit.title('My parents New Healthy Diner'),
-streamlit.subtitle('Breakfastmenu'),
-streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.title('My parents New Healthy Diner')
