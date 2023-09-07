81592cdd3b3880a978a408d886711ec6faad02ae
adriTIL <138571212+adriTIL@users.noreply.github.com>
2023-09-07 12:13:27 +0200

    Update streamlit_app.py
    Text update and pandas fruit list imported

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit
+import pandas
 
-streamlit.title('My parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach and rocket')
-streamlit.text('Hard-bioled egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
